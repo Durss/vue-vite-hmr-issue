10e1dd6cbdd3d1c7812f4b90c59300dd9f8bbd35
Durss <francois.dursus@gmail.com>
2023-12-03 16:26:22 +0100

    base commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="app">Hello world!</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-facing-decorator';
+
+@Component
+export default class App extends Vue {
+
+}
+</script>
+
+<style scoped>
+.app{
+}
+</style>
